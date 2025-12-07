2d2385e20e0e971d732fc5117a7e085a40def64f
Lior Kesos <lior@hossted.com>
2025-12-07 13:16:51 +0200

    ci and lock

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 ## The "save to chat" overwrites the github ci and npm lock
 ## Add the github workflow 
 mkdir -p .github/workflows
-cp ./ci/deploy-pages.yml .github/workflows
+cp ./deploy-pages.yml .github/workflows
+git add .github/workflows/deploy-pages.yml
 
 
 ## Create npm.lock
 npm install 
 git add package-lock.json
-git commit -am "Adding lock"
+git commit -am "ci and lock"
